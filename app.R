cec32f4838e79dab0a0b9aed7c02f8de596433f5
HarunCelikOtto <hcelik@iastate.edu>
2023-04-18 20:15:43 -0500

    initial commit

---
+++ app.R
@@ -0,0 +1,46 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+library(dplyr)
+
+# Define UI for application that draws a histogram
+ui <- fillPage(
+  navbarPage("Historical Mapping", 
+             id="nav",
+             
+    tabPanel(title = "Interactive Map",
+             div(class="outer",
+                
+             tags$head(
+               includeCSS("styles.css")
+             ),
+             
+             
+             leafletOutput("raster_map", 
+                           width = "100%", 
+                           height = "100%")
+               ))))
+             
+             
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+     output$raster_map <- renderLeaflet({
+       leaflet() %>%
+         addTiles() %>%
+         leafletOptions(setView(lng = 92.4303707, lat = 42.4000755, zoom = 12))
+     })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
